869da3c586e45120ff38f1976a321f0305e9852d
Bigbossmskub <mokamoliza@gmail.com>
2025-12-10 14:14:17 +0700

    lastest

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ import ProtectedRoute from "./components/auth/ProtectedRoute";
 
 function AppContent() {
   const location = useLocation();
-  const hideHeaderPaths = ["/auth"]; // Only hide on auth page
+  const hideHeaderPaths = ["/auth"];
 
   return (
     <div className="app">
